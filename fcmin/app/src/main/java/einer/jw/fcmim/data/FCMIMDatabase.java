e4614af6d0f9915dbcd6a8efd93c3125da5c0a89
Einer Santana R <einersantanar@hotmail.com>
2016-01-31 22:34:24 -0400

    Added sql of table creation
    The sql that creates the primary tables was added and staring to code
    the first class that will provide the data of the server location

--- fcmin/app/src/main/java/einer/jw/fcmim/data/FCMIMDatabase.java
+++ fcmin/app/src/main/java/einer/jw/fcmim/data/FCMIMDatabase.java
@@ -1,8 +1,13 @@
 package einer.jw.fcmim.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Einer on 31/1/2016.
@@ -20,7 +25,44 @@ public class FCMIMDatabase extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-       // String users = "CREATE TABLE IF NOT EXISTS auth("
+        /*
+        * Defined the authentication method
+        * 1 - Web service
+        * 2 - Local Server(Wifi)
+        * */
+        String users = "CREATE TABLE IF NOT EXISTS authentication_remote_method(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "auth_type INTEGER)";
+
+        // indicates the server location where the packages are going to be send
+        // and tells which location is going to be saved
+        String server_location = "CREATE TABLE IF NOT EXISTS server_location(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "url TEXT," +
+                "active INTEGER)";
+
+        /*
+        * Add cache information of the application as string data.
+        * */
+        String cache = "CREATE TABLE IF NOT EXISTS cache(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "cache_key VARCHAR(50)," +
+                "data TEXT)";
+
+
+
+        // register logs
+        String logs = "CREATE TABLE IF NOT EXISTS logs(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "caption TEXT," +
+                "message TEXT," +
+                "date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+
+        db.execSQL(users);
+        db.execSQL(server_location);
+        db.execSQL(cache);
+        db.execSQL(logs);
+
 
     }
 
@@ -28,4 +70,79 @@ public class FCMIMDatabase extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    public static class Settings
+    {
+        private SQLiteOpenHelper db;
+
+
+
+        private Settings(SQLiteOpenHelper db)
+        {
+              this.db = db;
+        }
+
+        public List<ServerLocation> getServersLocations()
+        {
+            SQLiteDatabase wdb = this.db.getReadableDatabase();
+            Cursor cursor = wdb.rawQuery("SELECT * FROM server_locations", null);
+
+            List<ServerLocation> serverLocations = new ArrayList<>();
+            while (cursor.moveToNext())
+            {
+                serverLocations.add(toServerLocation(cursor));
+            }
+
+            return serverLocations;
+        }
+
+        private ServerLocation toServerLocation(Cursor cursor)
+        {
+            ServerLocation serverLocation = new ServerLocation();
+            String url = cursor.getString(cursor.getColumnIndex("url"));
+            serverLocation.setUrl(url);
+            serverLocation.setActive(cursor.getInt(cursor.getColumnIndex("active")) == 1);
+            serverLocation.setId(cursor.getInt(cursor.getColumnIndex("id")));
+
+            return serverLocation;
+        }
+
+        public ServerLocation getActiveServerLocation()
+        {
+            SQLiteDatabase rdb = db.getReadableDatabase();
+            Cursor cursor = rdb.rawQuery("SELECT * FROM server_location WHERE is_active = 1", null);
+
+            if (cursor.moveToNext())
+            {
+                return toServerLocation(cursor);
+            }
+            else{
+                return null;
+            }
+        }
+
+
+        public void setActiveServer(ServerLocation server)
+        {
+
+            SQLiteDatabase rdb = this.db.getReadableDatabase();
+            Cursor cursor = rdb.rawQuery("SELECT count(1) FROM server_location WHERE url ='" + server + "'",  null);
+
+            if (cursor.moveToFirst())
+            {
+                boolean exists = cursor.getInt(0) >= 1;
+                if (exists)
+                {
+
+                    SQLiteDatabase wrb = this.db.getWritableDatabase();
+
+                    ContentValues contentValues = new ContentValues();
+                    // TODO finish the update command here of the server location
+                }
+            }
+
+        }
+
+    }
+
 }
